1d027dd2fac8ca4f3393f46e5df0b9bec7ebb4f1
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-02-21 11:31:19 +0100

    08-constructor-add

--- 03_code/snack_vending_machine/src/ch/noseryoung/blj/Item.java
+++ 03_code/snack_vending_machine/src/ch/noseryoung/blj/Item.java
@@ -2,8 +2,24 @@ package ch.noseryoung.blj;
 
 public class Item {
 
-    public static void busFahren () {
-        System.out.println("Busfahren");
-    }
+    public String name;
+    public String description;
+    public double price;
+    public int maxQuantity;
+    public int quantity;
 
+    public Item(String name, String description, double price, String maxQuantity, int quantity) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        if (maxQuantity.equals("default")) {
+            this.maxQuantity = 64;
+        } else if (maxQuantity.equals("limited")) {
+            this.maxQuantity = 10;
+        } else {
+            System.err.println("ERROR: Invalid max quantity! Value was set do default!");
+            this.maxQuantity = 64;
+        }
+        this.quantity = quantity;
+    }
 }
